1e516cd1d551114661bc5656dd55f965150217af
Bruno Banaszczyk <bruno.banaszczyk@gmail.com>
2025-02-28 22:31:15 +0100

    expanded User class, added password hashing

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,22 @@
-from main import db
+from main import db, bcrypt
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
-from flask_bcrypt import bcrypt
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = "app_user"
+    
     user_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     username = db.Column(db.String(32), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.now(datetime.timezone.utc), nullable=False)
+    
+    def __init__(self, username, password, email):
+        self.username = username
+        self.email = email # TODO! Add function that checks email using regex
+        pass_hash = bcrypt.generate_password_hash(password)
+        self.password_hash = pass_hash
 
     def __repr__(self):
         return f"User {self.username}"
