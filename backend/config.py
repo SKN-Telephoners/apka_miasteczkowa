d0618ba07ae44bb47e6e9ac578bb60b037c21630
Bruno Banaszczyk <bruno.banaszczyk@gmail.com>
2025-03-24 13:22:06 +0100

    Register 2 (#5)
    * registration work
    
    * read me
    
    * fixed errors, finished registration, added tests
    
    * removing readme
    
    ---------
    
    Co-authored-by: jan pieprzycki <pieprzycki.jan@gmail.com>

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,3 @@
-import os
-
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/apka_miasteczkowa?sslmode=require"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/routes.py
+++ backend/routes.py
@@ -1,11 +1,52 @@
 from flask import Blueprint, request, jsonify
 from backend.models import User
 from backend.extensions import db
+import re
+
+MAX_EMAIL_LEN = 320
+MAX_USERNAME_LEN = 32
+MIN_USERNAME_LEN = 3
 
 main = Blueprint("main", __name__)
 auth = Blueprint("auth", __name__)
 
+
 public_url = "example address"
+
+@auth.route("/api/register",methods=["POST"])
+def register_user():    
+    user_data = request.get_json()
+    required_keys = {"username", "password", "email"}
+    print(user_data)
+
+    if not user_data or not required_keys.issubset(user_data.keys()):
+        return jsonify({"message": "Bad request"}), 400
+    
+    username = user_data["username"]
+    password = user_data["password"]
+    email = user_data["email"]
+    
+    email_pattern = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+    if (
+        not re.match(email_pattern, email)
+        or not len(email) < MAX_EMAIL_LEN
+        or not MIN_USERNAME_LEN <= len(username) <= MAX_USERNAME_LEN
+    ):
+        return jsonify({"message": "Invalid username or email"}), 400
+    
+    new_user = User(username=username, password=password, email=email)
+    if db.session.query(User).filter_by(username=username).first() is not None:
+        return jsonify({"message": "Username already taken"}), 409
+    if db.session.query(User).filter_by(email=email).first() is not None:
+        return jsonify({"message": "Account with this email already exists"}), 409
+    
+    db.session.add(new_user)
+    db.session.commit()
+    print("Dodano użytkownika do bazy!")
+    
+    return {
+        "message": "Registration successful",
+    }, 200
 
 @auth.route("/api/login", methods=["POST"])
 def login_user():
