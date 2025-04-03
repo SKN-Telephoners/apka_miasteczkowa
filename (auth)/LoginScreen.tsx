a7cfa7afa5b8b08262b4d651e6fc639bb54270b6
anuszek <153279378+anuszek@users.noreply.github.com>
2025-04-03 15:22:07 +0200

    alerts for login

--- (auth)/LoginScreen.tsx
+++ (auth)/LoginScreen.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
-  View,
+  Alert,
+  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
-  StyleSheet,
-  Alert,
+  View,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import axios from "axios";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const BACKEND_URL = "http://10.0.2.2:5000";
 
@@ -30,66 +30,88 @@ const LoginScreen = ({ navigation }: { navigation: any }) => {
     logIn(username, password);
   };
 
-//funkcja login 
-async function logIn(username: string, password: string): Promise<void> {
-  try {
-    const response = await axios.post('http://10.0.2.2:5000/api/login', {
-      username: username,
-      password: password,
-    });
+  //funkcja login
+  async function logIn(username: string, password: string): Promise<void> {
+    try {
+      const response = await axios.post("http://10.0.2.2:5000/api/login", {
+        username: username,
+        password: password,
+      });
 
-    if (response.status === 200) {
-      console.log("Logowanie powiodło się!");
+      if (response.status === 200) {
+        console.log("Logowanie powiodło się!");
 
-      const { access_token, refresh_token } = response.data; // assuming the token is returned as 'token'
-      await AsyncStorage.setItem('access_token', access_token);
-      await AsyncStorage.setItem('refresh_token', refresh_token);
+        const { access_token, refresh_token } = response.data; // assuming the token is returned as 'token'
+        await AsyncStorage.setItem("access_token", access_token);
+        await AsyncStorage.setItem("refresh_token", refresh_token);
 
-      navigation.navigate('Home');
-    } else {
-      console.log("Logowanie nie powiodło się. Kod:", response.status);
-    }
-  } catch (error: any) {
-    if (error.response) {
-      console.log("Logowanie nie powiodło się. Kod:", error.response.status);
-    } else {
-      console.error("Błąd:", error.message);
+        navigation.navigate("Home");
+      } else {
+        console.log("Logowanie nie powiodło się. Kod:", response.status);
+      }
+    } catch (error: any) {
+      if (error.response) {
+        console.log("Logowanie nie powiodło się. Kod:", error.response.status);
+      } else {
+        console.error("Błąd:", error.message);
+      }
     }
   }
-}
 
   return (
-    <View style={styles.container}>
+    <View style={styles.mainContainer}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Aplikacja Miasteczkowa</Text>
       </View>
 
-      <View style={styles.inputContainer}>
-        <Ionicons name="person-outline" size={20} color="#ff914d" />
-        <TextInput
-          placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
-          style={styles.input}
-        />
-      </View>
+      <View>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputBox}>
+            <Ionicons name="person-outline" size={20} color="#ff914d" />
+            <TextInput
+              placeholder="Username"
+              value={username}
+              onChangeText={setUsername}
+              style={styles.input}
+            />
+          </View>
+          <Text
+            style={[
+              styles.errorMessage,
+              { display: message ? "flex" : "none" },
+            ]}
+          >
+            {}
+          </Text>
+        </View>
 
-      <View style={styles.inputContainer}>
-        <Ionicons name="lock-closed-outline" size={20} color="#ff914d" />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry={secureText}
-          style={styles.input}
-        />
-        <TouchableOpacity onPress={() => setSecureText(!secureText)}>
-          <Ionicons
-            name={secureText ? "eye-off-outline" : "eye-outline"}
-            size={20}
-            color="#ff914d"
-          />
-        </TouchableOpacity>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputBox}>
+            <Ionicons name="lock-closed-outline" size={20} color="#ff914d" />
+            <TextInput
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={secureText}
+              style={styles.input}
+            />
+            <TouchableOpacity onPress={() => setSecureText(!secureText)}>
+              <Ionicons
+                name={secureText ? "eye-off-outline" : "eye-outline"}
+                size={20}
+                color="#ff914d"
+              />
+            </TouchableOpacity>
+          </View>
+          <Text
+            style={[
+              styles.errorMessage,
+              { display: message ? "flex" : "none" },
+            ]}
+          >
+            {}
+          </Text>
+        </View>
       </View>
 
       <TouchableOpacity onPress={() => navigation.navigate("ResetPassword")}>
@@ -111,38 +133,45 @@ async function logIn(username: string, password: string): Promise<void> {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#f5f5f5",
   },
   titleContainer: {
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 50
+    marginBottom: 50,
   },
   title: {
     fontSize: 48,
     fontWeight: "bold",
     color: "#004aad",
-    textAlign: "center"
+    textAlign: "center",
   },
   inputContainer: {
+    marginBottom: 15,
+    // width: "80%",
+  },
+  inputBox: {
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#fff",
     borderRadius: 10,
     paddingHorizontal: 10,
-    marginBottom: 15,
-    width: "80%",
     height: 50,
     borderWidth: 1,
-    borderColor: "#ccc"
+    borderColor: "#ccc",
   },
   input: {
     flex: 1,
-    marginLeft: 10
+    marginLeft: 10,
+  },
+  errorMessage: {
+    color: "red",
+    marginTop: 5,
+    // display: "none"
   },
   loginButton: {
     backgroundColor: "#4a90e2",
@@ -151,7 +180,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: "center",
     marginTop: 50,
-    marginBottom: 10
+    marginBottom: 10,
   },
   signUpButton: {
     backgroundColor: "#ff914d",
@@ -159,22 +188,22 @@ const styles = StyleSheet.create({
     width: "80%",
     borderRadius: 10,
     alignItems: "center",
-    marginTop: 10
+    marginTop: 10,
   },
   buttonText: {
     color: "#fff",
-    fontSize: 18
+    fontSize: 18,
   },
   forgotPassword: {
     marginTop: 10,
-    color: "#4a90e2"
+    color: "#4a90e2",
   },
   wifi: {
     position: "absolute",
     top: 150,
     right: 95,
-    color: "#4a90e2"
-  }
+    color: "#4a90e2",
+  },
 });
 
 export default LoginScreen;
