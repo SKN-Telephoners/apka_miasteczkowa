9438538edbe0395b68342920d981d9091f2cff69
jan pieprzycki <pieprzycki.jan@gmail.com>
2025-04-03 22:14:32 +0200

    add new comllumn

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,13 @@
 from datetime import timedelta
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/apka_miasteczkowa?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ZAQ!%40WSX@localhost/apka_miasteczkowa?sslmode=require"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "" #provide secret key
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/apka_miasteczkowa_test?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ZAQ!%40WSX@localhost/apka_miasteczkowa_test?sslmode=require"
     TESTING = True
 
