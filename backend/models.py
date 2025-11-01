a0ab3506fc967c599397ec7c39890fe638e94278
mieentus <kolodziejska.m@outlook.com>
2025-11-01 17:59:40 +0100

    Events (#16)
    * DB Connecting Test
    
    * added requirements.txt for python venv
    
    * connection with DB, added tests, new schema
    
    * updated to npm 11, so package-lock has changed
    
    * prepared branch for API development
    
    * updating pip requirements.txt
    
    * Inital user login endpoint with unit tests (#3)
    
    * added initial login endpoint
    
    * added tests for login endpoint
    
    * updating .gitignore
    
    * zrobione wyslanie api do rejestracji i logowania (nie testowane) (#4)
    
    Co-authored-by: pasiek <ppasieka@student.agh.edu.pl>
    
    * poprawka homescreen
    
    * new tables and handling friend requests + tests
    
    * event model and basic routes (create and delet) with tests
    
    ---------
    
    Co-authored-by: Mikil123 <heartsofiron4@interia.pl>
    Co-authored-by: Bruno Banaszczyk <bruno.banaszczyk@gmail.com>
    Co-authored-by: pasiek <ppasieka@student.agh.edu.pl>
    Co-authored-by: Patryk <patryk@pasieka.com>
    Co-authored-by: Marta Kolodziejska <kolodziejska@marta.com>

--- backend/models.py
+++ backend/models.py
@@ -77,3 +77,20 @@ class FriendRequest(db.Model):
 
     sender = db.relationship('User', foreign_keys=[sender_id])
     receiver = db.relationship('User', foreign_keys=[receiver_id])
+
+class Event(db.Model):
+    __tablename__ = "event"
+    
+    event_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    name = db.Column(db.String(32), nullable=False)
+    description = db.Column(db.String(1000))
+    date_and_time = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
+    location = db.Column(db.String(32), nullable=False)
+    creator_id = db.Column(UUID(as_uuid=True), db.ForeignKey("app_user.user_id", ondelete='CASCADE'), nullable=False, index=True)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
+
+    __table_args__ = (
+        CheckConstraint('date > CURRENT_TIMESTAMP', name='check_event_date_future'),
+    )
+
+    creator = db.relationship("User", foreign_keys=[creator_id])
