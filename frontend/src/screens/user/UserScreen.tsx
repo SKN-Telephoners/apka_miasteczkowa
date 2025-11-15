109860e9c710e50db25213706b3d1d68cf00a085
2xmiau <plukaszyk@student.agh.edu.pl>
2025-11-15 20:51:18 +0100

    Initial commit

---
+++ frontend/src/screens/user/UserScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const UserScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Proszę nie patrzeć jeszcze</Text>
+    </View>
+  );
+};
+
+export default UserScreen;
