f02abafea5b3759b371e972364a76a02b5a4c93e
2xmiau <plukaszyk@student.agh.edu.pl>
2025-11-22 15:29:34 +0100

    add clear asyncstorage

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { tokenStorage } from "../utils/storage";
 
 interface AuthContextType {
@@ -40,6 +41,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const logout = async () => {
     await tokenStorage.clearTokens();
+    await AsyncStorage.clear();
     setIsAuthenticated(false);
   };
 
