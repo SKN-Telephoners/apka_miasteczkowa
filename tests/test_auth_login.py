e4eb803c94f6883669dcde38ab29ab0b9f2a9a68
Jasiopi <pieprzycki.jan@gmail.com>
2025-11-28 22:05:01 +0100

    konflikty_2

--- tests/test_auth_login.py
+++ tests/test_auth_login.py
@@ -148,6 +148,67 @@ def test_jwt_revoke_refresh_token(client, registered_user, app):
         assert refresh_response.status_code == 401
 
 # =============================================================================
+# Tests for auth user
+# =============================================================================
+
+def test_auth_user(client, app):
+    with app.app_context():
+        username = "test_user"
+        password = "123456flask"
+        email = "test_user@gmail.com"
+        user = User(username=username, password=password, email=email)
+
+        
+        db.session.add(user)
+        db.session.commit()
+
+        assert user.is_confirmed is False
+
+        email_payload = {
+            "email": email
+        }
+
+        with mail.record_messages() as outbox:
+            response = client.post("/mail_auth_request", json=email_payload)
+
+            assert response.status_code == 200
+            assert len(outbox) == 1
+            assert outbox[0].subject == 'Auth account'
+
+            match = search(r'(http://.+/mail_auth/\S+)', outbox[0].body)
+            assert match, "No auth URL found in email body"
+
+            auth_url = match.group()
+            token_path = auth_url.replace("http://localhost", "")
+            
+            assert response.status_code == 200 
+
+            response = client.post(token_path)
+            assert response.status_code == 200
+            assert response.get_json()["message"] == "Verification succesful"
+
+
+        user_answer=User.query.filter_by(email=email).first()
+        assert user_answer.is_confirmed is True
+
+        db.session.rollback()
+        db.session.query(User).delete()
+        db.session.commit()
+
+def test_auth_user_invalid_email(client, app):
+    with app.app_context():
+        email_payload = {
+            "email": "invalid_user@gmail.com"
+        }
+
+        with mail.record_messages() as outbox:
+            response = client.post("/mail_auth_request", json=email_payload)
+
+            assert response.status_code == 401
+            assert len(outbox) == 0
+
+
+# =============================================================================
 # Tests for user logout
 # =============================================================================
 
