b8b787b5e76b255e61d964f4b598210db6d0c3f3
Jasiopi <pieprzycki.jan@gmail.com>
2025-12-04 23:08:06 +0100

    add readme

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+4.12.2025
+Wcześniejsza gałąź feed_view mi się zepsułą i nie chciałem jej już naprawiać XD
+
+jest dodana funkcja 
+feed()
+w routes.py która zwraca 21 eventów
+
+jest zrobiny zalązeg testów w pliku test_feed.py
+
+w conftest.py
+dodałęm fixture który tworzy 21 eventów do testów
---
+++ tests/test_feed.py
@@ -0,0 +1,74 @@
+import pytest
+from re import search
+from backend.extensions import mail ,db 
+from backend.models import User
+
+# =============================================================================
+# Tests for feed
+# =============================================================================
+
+
+
+
+def test_feed_returns_events(client,create_event):
+    response = client.get("/feed")
+
+    assert response.status_code == 200
+"""
+    data = response.get_json()
+    assert "data" in data
+    assert "pagination" in data
+
+    events = data["data"]
+    pagination = data["pagination"]
+
+    assert isinstance(events, list)
+    assert len(events) == len(sample_events)
+
+    assert pagination["page"] == 1
+    assert pagination["limit"] == 20
+    assert pagination["total"] == len(sample_events)
+    assert pagination["pages"] == 1  # bo wszystkich eventów ≤ limit
+
+    # sprawdzenie czy eventy są posortowane rosnąco po dacie
+    dates = [
+        datetime.strptime(e["date"] + " " + e["time"], "%d.%m.%Y %H:%M")
+        for e in events
+    ]
+    assert dates == sorted(dates)
+
+
+
+def test_feed_pagination(client, access_token, sample_events):
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = client.get("/feed?page=1&limit=2", headers=headers)
+    assert response.status_code == 200
+
+    data = response.get_json()
+    assert "data" in data
+    assert "pagination" in data
+
+    events = data["data"]
+    pagination = data["pagination"]
+
+    assert len(events) == 2  # limit = 2
+    assert pagination["page"] == 1
+    assert pagination["limit"] == 2
+    assert pagination["total"] == len(sample_events)
+    assert pagination["pages"] == (len(sample_events) + 1) // 2  # zaokrąglenie w górę
+
+
+
+
+def test_feed_empty(client, access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = client.get("/feed", headers=headers)
+    assert response.status_code == 200
+
+    data = response.get_json()
+    assert data["data"] == []
+    assert data["pagination"]["total"] == 0
+    assert data["pagination"]["pages"] == 0
+"""
