8352e9666e38aa8fc63174715b71d84d6d61236b
anuszek <153279378+anuszek@users.noreply.github.com>
2025-12-13 12:46:48 +0100

    Integrate MapLibre

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -91,11 +91,13 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {!isAuthenticated ? (
+        {/* {!isAuthenticated ? (
           <Stack.Screen name="Auth" component={AuthStack} />
         ) : (
           <Stack.Screen name="Main" component={MainTabs} />
-        )}
+        )} */}
+        {/* Temporary disable auth flow for development */}
+        <Stack.Screen name="Main" component={MainTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- frontend/src/screens/home/MapScreen.tsx
+++ frontend/src/screens/home/MapScreen.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import MapLibreGL from "@maplibre/maplibre-react-native";
 
 const MapScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View>
       <Text>Placeholder mapy</Text>
     </View>
   );
