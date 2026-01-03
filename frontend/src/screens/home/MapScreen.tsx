2800ee9466aa43aa20d33a4ccc46d6aa77d429c0
anuszek <153279378+anuszek@users.noreply.github.com>
2026-01-03 13:20:35 +0100

    raster map for development

--- frontend/src/screens/home/MapScreen.tsx
+++ frontend/src/screens/home/MapScreen.tsx
@@ -1,16 +1,22 @@
+import {
+  Camera,
+  MapView,
+  RasterLayer,
+  RasterSource,
+  setAccessToken,
+} from "@maplibre/maplibre-react-native";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { MapView, Camera, setAccessToken } from "@maplibre/maplibre-react-native";
 
 setAccessToken(null);
 
-export default function MapScreen() {
+const MapScreen = () => {
   return (
     <View style={styles.page}>
-      <MapView
+      {/*target implementation - vector rendering */}
+      {/* <MapView
         style={styles.map}
-        // Use the vector style. Note: "streets" (no version) is safest.
-        styleURL="https://api.maptiler.com/maps/streets/style.json?key=6TeNoAO6gd5SoLzdLgGf"
+        styleURL="https://api.maptiler.com/maps/streets/style.json?key=MAPTILER_KEY"
         logoEnabled={false}
       >
         <Camera
@@ -19,12 +25,38 @@ export default function MapScreen() {
           animationMode="flyTo"
           animationDuration={2000}
         />
+      </MapView> */}
+
+      {/*OpenStreetMap Tiles via RasterSource and RasterLayer for development*/}
+      <MapView style={styles.map} logoEnabled={false}>
+        <Camera
+          zoomLevel={14}
+          centerCoordinate={[19.9449, 50.0647]}
+          animationMode="moveTo"
+        />
+
+        {/* OpenStreetMap Images */}
+        <RasterSource
+          id="osmSource"
+          tileUrlTemplates={[
+            "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          ]}
+          tileSize={256}
+        />
+
+        <RasterLayer
+          id="osmLayer"
+          sourceID="osmSource"
+          style={{ rasterOpacity: 1 }}
+        />
       </MapView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   page: { flex: 1 },
   map: { flex: 1 },
 });
+
+export default MapScreen;
