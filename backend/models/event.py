9ca3945a589c48c921466e539f2e95d21d0d48d9
Natalia <natalia.61b@gmail.com>
2026-03-19 19:09:46 +0100

    events model

--- backend/models/event.py
+++ backend/models/event.py
@@ -8,13 +8,16 @@ class Event(db.Model):
     __tablename__ = "Event"
     
     event_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    name = db.Column(db.String(32), nullable=False)
+    event_name = db.Column(db.String(32), nullable=False)
     description = db.Column(db.String(1000))
     date_and_time = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
     location = db.Column(db.String(32), nullable=False)
     creator_id = db.Column(UUID(as_uuid=True), db.ForeignKey("User.user_id", ondelete='CASCADE'), nullable=False, index=True)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
-    edited = db.Column(db.Boolean, default=False)
+    is_edited = db.Column(db.Boolean, default=False)
+    comment_count = db.Column(db.Int, default=0, nullable = False)
+    participant_count = db.Column(db.Int, default=0, nullable = False)
+    is_private = db.Column(db.Boolean, default = False, nullable = False)
 
     __table_args__ = (
         CheckConstraint('date_and_time > CURRENT_TIMESTAMP', name='check_event_date_future'),
