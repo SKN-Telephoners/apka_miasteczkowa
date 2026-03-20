0b9f9fe6d1bc16956e48161690c626bdc89e23db
Natalia <natalia.61b@gmail.com>
2026-03-20 19:05:17 +0100

    counting comments

--- backend/routes/comment_routes.py
+++ backend/routes/comment_routes.py
@@ -17,7 +17,6 @@ def create_comment(event_id):
     e_uuid = validate_uuid(event_id)
     if not e_uuid:
         return make_api_response(ResponseTypes.INVALID_DATA, message="Invalid event ID")
-
     event = db.session.get(Event, e_uuid)
 
     if not event:
@@ -25,6 +24,8 @@ def create_comment(event_id):
     
     comment_data = request.get_json(silent=True)
     required_keys = {"content"}
+    comment_count = event.comment_count 
+    comment_count += 1
     
     if not comment_data or not required_keys.issubset(comment_data.keys()):
         return make_api_response(ResponseTypes.BAD_REQUEST, message="Missing content")
@@ -38,6 +39,7 @@ def create_comment(event_id):
     
     try:
         new_comment = Comment(user_id=user.user_id, event_id=event_id, content=comment_data["content"])
+        event.comment_count = comment_count
 
         db.session.add(new_comment)
         db.session.commit()
@@ -52,18 +54,29 @@ def create_comment(event_id):
 @comments_bp.route("/reply/<parent_comment_id>", methods=["POST"])
 @jwt_required()
 @limiter.limit("90 per minute")
-def reply_to_comment(parent_comment_id):
+def reply_to_comment(parent_comment_id , event_id):
     user = get_current_user()
     p_uuid = validate_uuid(parent_comment_id)
+    e_uuid = validate_uuid(event_id)
+    
     if not p_uuid:
         return make_api_response(ResponseTypes.INVALID_DATA, message="Invalid parent comment ID")
     parent_comment = Comment.query.filter_by(comment_id=parent_comment_id).first()
+
+    if not e_uuid:
+        return make_api_response(ResponseTypes.INVALID_DATA, message="Invalid event ID")
+    event = Event.query.filter_by(event_id=event_id).first()
+
+    if event is None:
+        return make_api_response(ResponseTypes.NOT_FOUND, message="Event doesn't exist")
 
     if parent_comment is None:
         return make_api_response(ResponseTypes.NOT_FOUND, message="Parent comment doesn't exist")
     
     comment_data = request.get_json(silent=True)
     required_keys = {"content"}
+    comment_count = event.comment_count
+    comment_count += 1
     
     if not comment_data or not required_keys.issubset(comment_data.keys()):
         return make_api_response(ResponseTypes.BAD_REQUEST, message="Missing content")
@@ -82,6 +95,7 @@ def reply_to_comment(parent_comment_id):
             content=comment_data["content"],
             parent_comment_id=parent_comment_id
             )
+        event.comment_count = comment_count
 
         db.session.add(new_comment)
         db.session.commit()
@@ -95,21 +109,32 @@ def reply_to_comment(parent_comment_id):
 @comments_bp.route("/delete/<comment_id>", methods=["DELETE"])
 @jwt_required()
 @limiter.limit("90 per minute")
-def delete_comment(comment_id):
+def delete_comment(comment_id, event_id):
     user = get_current_user()
     c_uuid = validate_uuid(comment_id)
+    e_uuid = validate_uuid(event_id)
+
     if not c_uuid:
-        return make_api_response(ResponseTypes.INVALID_DATA, message="Invalid UUID format")
+        return make_api_response(ResponseTypes.INVALID_DATA, message="Invalid comment ID format")
+    
+    if not e_uuid:
+        return make_api_response(ResponseTypes.INVALID_DATA, message="Invalid event ID format")
     
     comment = Comment.query.filter_by(comment_id=comment_id).first()
+    event = Event.query.filter_by(event_id=event_id).first()
+
     if comment is None:
         return make_api_response(ResponseTypes.NOT_FOUND, message="Comment doesn't exist")
 
     if user.user_id != comment.user_id:
         return make_api_response(ResponseTypes.FORBIDDEN, message="You can delete your own comments only")
+    
+    comment_count = event.comment_count
+    comment_count += 1
         
     try: 
         comment.soft_delete()
+        event.comment_count = comment_count
         db.session.commit()
     except SQLAlchemyError as e:
         db.session.rollback()
