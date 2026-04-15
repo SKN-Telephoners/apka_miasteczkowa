c0227ea287c5835596b89d273e1994447e2d28d3
Maurycy <153279378+anuszek@users.noreply.github.com>
2026-04-15 21:05:26 +0200

    location picker

--- frontend/src/screens/home/MapScreen.tsx
+++ frontend/src/screens/home/MapScreen.tsx
@@ -174,6 +174,7 @@ export default function MapScreen() {
   );
 }
 
+// needs unified styles
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center" },
   mapcontainer: { width: "100%", height: "100%", position: "relative" },
